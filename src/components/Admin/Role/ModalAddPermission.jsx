6cc529a8152fce4a1a18d625cccc3c3111a52a47
Đặng Ngọc Tài <2001224231@hufi.edu.vn>
2025-03-17 23:16:31 +0700

    fixAddPermission

--- src/components/Admin/Role/ModalAddPermission.jsx
+++ src/components/Admin/Role/ModalAddPermission.jsx
@@ -1,61 +1,94 @@
-import React, { useEffect, useState } from 'react';
-import { Button, DatePicker, Divider, Form, Input, InputNumber, message, Modal, notification, Select } from 'antd';
-import { callCreateAUser, callCreateCustomer, callCreateEmployee, callFetchBookById, callFetchPermissionById, callFetchPermissionByName, callListRole } from '../../../services/api';
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  DatePicker,
+  Divider,
+  Form,
+  Input,
+  InputNumber,
+  message,
+  Modal,
+  notification,
+  Select,
+} from "antd";
+import {
+  callCreateAUser,
+  callCreateCustomer,
+  callCreateEmployee,
+  callFetchBookById,
+  callFetchPermissionById,
+  callFetchPermissionByName,
+  callListRole,
+} from "../../../services/api";
 
 const ModalAddPermission = (props) => {
-    const { openModalAddPermission, setModalAddPermission, listPermissions, products, setProducts } = props;
-    const [form] = Form.useForm();
+  const {
+    openModalAddPermission,
+    setModalAddPermission,
+    listPermissions,
+    products,
+    setProducts,
+    permissionDataUpdate,
+  } = props;
+  const [form] = Form.useForm();
 
-
-
-
-
-    const onFinish = async (values) => {
-
-        const { permission } = values;
-        const res = await callFetchPermissionByName(permission);
-        console.log('res', res);
-        if (res && res.data) {
-            const { id, name, apiPath, method, module } = res.data;
-            const pr = { id, name, apiPath, method, module };
-            setProducts([...products, { ...pr, key: Date.now() }]);
-            setModalAddPermission(false);
-        }
-        form.resetFields();
-    };
-
-
-    return (
-        <>
-            {/* Modal thêm sản phẩm */}
-            <Modal
-                title="Thêm permission vào role"
-                open={openModalAddPermission}
-                onCancel={() => { setModalAddPermission(false); form.resetFields() }}
-                onOk={() => { form.submit() }}
-            >
-                <Form layout="vertical"
-                    form={form}
-                    name="add_permission_form"
-                    onFinish={onFinish}
-
-                >
-                    <Form.Item
-                        label="Tên quyền"
-                        name="permission"
-                        rules={[{ required: true, message: 'Vui lòng chọn quyền!' }]}
-                    >
-                        <Select
-                            defaultValue={null}
-                            showSearch
-                            allowClear
-                            options={listPermissions}
-                        />
-                    </Form.Item>
-                </Form>
-            </Modal>
-        </>
+  // Lọc danh sách quyền để loại bỏ các quyền đã có trong role
+  const filteredPermissions = listPermissions.filter((permission) => {
+    // Kiểm tra xem permission này đã tồn tại trong permissionDataUpdate chưa
+    return !permissionDataUpdate?.some(
+      (existingPerm) => existingPerm.name === permission.value
     );
+  });
+
+  const onFinish = async (values) => {
+    const { permission } = values;
+    const res = await callFetchPermissionByName(permission);
+    console.log("res", res);
+    if (res && res.data) {
+      const { id, name, apiPath, method, module } = res.data;
+      const pr = { id, name, apiPath, method, module };
+      setProducts([...products, { ...pr, key: Date.now() }]);
+      setModalAddPermission(false);
+    }
+    form.resetFields();
+  };
+
+  return (
+    <>
+      {/* Modal thêm sản phẩm */}
+      <Modal
+        title="Thêm permission vào role"
+        open={openModalAddPermission}
+        onCancel={() => {
+          setModalAddPermission(false);
+          form.resetFields();
+        }}
+        onOk={() => {
+          form.submit();
+        }}
+      >
+        <Form
+          layout="vertical"
+          form={form}
+          name="add_permission_form"
+          onFinish={onFinish}
+        >
+          <Form.Item
+            label="Tên quyền"
+            name="permission"
+            rules={[{ required: true, message: "Vui lòng chọn quyền!" }]}
+          >
+            <Select
+              defaultValue={null}
+              showSearch
+              allowClear
+              options={filteredPermissions}
+            />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  );
 };
 
 export default ModalAddPermission;
--- src/components/Admin/Role/RoleModalUpdate.jsx
+++ src/components/Admin/Role/RoleModalUpdate.jsx
@@ -1,190 +1,236 @@
-import React, { useEffect, useState } from 'react';
-import { Button, DatePicker, Divider, Form, Input, InputNumber, message, Modal, notification, Select, Table } from 'antd';
-import { callCreateAUser, callCreateCustomer, callCreateEmployee, callCreateImportReceipt, callCreateOrder, callCreateRole, callFetchAllPermission, callFetchBookBySupplierId, callFetchListBook, callFetchListBookNoPagination, callFetchListPermissionNoPagination, callFetchListSupplier, callFetchSupplierById, callFetcListShippingStatus, callListRole, callSupplyBySupplierIDAndBookID, callUpdateRole } from '../../../services/api';
-import moment from 'moment';
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  DatePicker,
+  Divider,
+  Form,
+  Input,
+  InputNumber,
+  message,
+  Modal,
+  notification,
+  Select,
+  Table,
+} from "antd";
+import {
+  callCreateAUser,
+  callCreateCustomer,
+  callCreateEmployee,
+  callCreateImportReceipt,
+  callCreateOrder,
+  callCreateRole,
+  callFetchAllPermission,
+  callFetchBookBySupplierId,
+  callFetchListBook,
+  callFetchListBookNoPagination,
+  callFetchListPermissionNoPagination,
+  callFetchListSupplier,
+  callFetchSupplierById,
+  callFetcListShippingStatus,
+  callListRole,
+  callSupplyBySupplierIDAndBookID,
+  callUpdateRole,
+} from "../../../services/api";
+import moment from "moment";
 
-import { useSelector } from 'react-redux';
-import { DeleteTwoTone } from '@ant-design/icons';
-import ModalAddPermission from './ModalAddPermission';
+import { useSelector } from "react-redux";
+import { DeleteTwoTone } from "@ant-design/icons";
+import ModalAddPermission from "./ModalAddPermission";
 
 const RoleModalUpdate = (props) => {
-    const { openModalUpdate, setOpenModalUpdate, dataUpdate, setDataUpdate } = props;
-    const [openModalAddPermission, setModalAddPermission] = useState(false);
-    const [isSubmit, setIsSubmit] = useState(false);
-    const [products, setProducts] = useState([]);
+  const { openModalUpdate, setOpenModalUpdate, dataUpdate, setDataUpdate } =
+    props;
+  const [openModalAddPermission, setModalAddPermission] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [products, setProducts] = useState([]);
 
+  const permissionDataUpdate = dataUpdate?.permissions;
 
-    // https://ant.design/components/form#components-form-demo-control-hooks
-    const [form] = Form.useForm();
+  // https://ant.design/components/form#components-form-demo-control-hooks
+  const [form] = Form.useForm();
 
-    const [listSupplier, setListSupplier] = useState([]);
-    const [listPermissions, setListPermissions] = useState([]);
+  const [listSupplier, setListSupplier] = useState([]);
+  const [listPermissions, setListPermissions] = useState([]);
 
-    const fetchListPermission = async () => {
-        const res = await callFetchListPermissionNoPagination();
-        if (res && res.data) {
-            const d = res.data?.map(item => {
-                return { label: item.name, value: item.name }
-            })
-            setListPermissions(d)
-        }
+  const fetchListPermission = async () => {
+    const res = await callFetchListPermissionNoPagination();
+    if (res && res.data) {
+      const d = res.data?.map((item) => {
+        return { label: item.name, value: item.name };
+      });
+      setListPermissions(d);
     }
+  };
 
-    useEffect(() => {
-        fetchListPermission();
-    }, []);
+  useEffect(() => {
+    fetchListPermission();
+  }, []);
 
-    useEffect(() => {
-        if (dataUpdate) {
-            console.log('dataUpdate', dataUpdate);
-            form.setFieldsValue({ ...dataUpdate })
-            const listPermissions = dataUpdate.permissions.map(permission => {
-                return { id: permission.id, name: permission.name, apiPath: permission.apiPath, method: permission.method, module: permission.module, key: permission.id }
-            })
+  useEffect(() => {
+    if (dataUpdate) {
+      console.log("dataUpdate", dataUpdate);
+      form.setFieldsValue({ ...dataUpdate });
+      const listPermissions = dataUpdate.permissions.map((permission) => {
+        return {
+          id: permission.id,
+          name: permission.name,
+          apiPath: permission.apiPath,
+          method: permission.method,
+          module: permission.module,
+          key: permission.id,
+        };
+      });
 
-            setProducts(listPermissions)
-        }
+      setProducts(listPermissions);
+    }
+  }, [dataUpdate]);
 
-    }, [dataUpdate]);
+  const onFinish = async (values) => {
+    const { id, name, description } = values;
+    console.log("values", values);
+    console.log("products", products);
+    const rqProducts = products.map((product) => {
+      return { id: product.id };
+    });
+    setIsSubmit(true);
+    console.log("rqProducts", rqProducts);
+    const res = await callUpdateRole(id, name, description, rqProducts);
+    if (res && res.data) {
+      message.success("Cập nhật role thành công");
+      form.resetFields();
+      setOpenModalUpdate(false);
+      await props.fetchRole();
+    } else {
+      notification.error({
+        message: "Đã có lỗi xảy ra",
+        description: res.message,
+      });
+    }
+    setIsSubmit(false);
+  };
+  const deleteProduct = (key) => {
+    setProducts(products.filter((product) => product.key !== key));
+  };
 
+  const productColumns = [
+    {
+      title: "Id",
+      dataIndex: "id",
+      key: "id",
+      width: "10%",
+    },
+    {
+      title: "Tên quyền",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "API",
+      dataIndex: "apiPath",
+      key: "apiPath",
+    },
+    {
+      title: "METHOD",
+      dataIndex: "method",
+      key: "method",
+    },
+    {
+      title: "MODULE",
+      dataIndex: "module",
+      key: "module",
+    },
+    {
+      title: "Thao tác",
+      key: "action",
+      render: (_, record) => (
+        <DeleteTwoTone
+          onClick={() => deleteProduct(record.key)}
+          twoToneColor="#ff4d4f"
+        />
+        // <Button type="link" onClick={() => deleteProduct(record.key)}>
+        //     Xóa
+        // </Button>
+      ),
+    },
+  ];
 
-    const onFinish = async (values) => {
-        const { id, name, description } = values;
-        console.log('values', values);
-        console.log('products', products);
-        const rqProducts = products.map(product => {
-            return { id: product.id }
-        })
-        setIsSubmit(true)
-        console.log('rqProducts', rqProducts);
-        const res = await callUpdateRole(id, name, description, rqProducts);
-        if (res && res.data) {
-            message.success('Cập nhật role thành công');
-            form.resetFields();
-            setOpenModalUpdate(false);
-            await props.fetchRole()
-        } else {
-            notification.error({
-                message: 'Đã có lỗi xảy ra',
-                description: res.message
-            })
-        }
-        setIsSubmit(false)
-    };
-    const deleteProduct = (key) => {
-        setProducts(products.filter((product) => product.key !== key));
-    };
+  return (
+    <>
+      <Modal
+        title="Cập nhật role"
+        open={openModalUpdate}
+        onOk={() => {
+          form.submit();
+        }}
+        onCancel={() => {
+          setOpenModalUpdate(false);
+          setProducts([]);
+          form.resetFields();
+          setDataUpdate(null);
+        }}
+        okText={"Tạo mới"}
+        cancelText={"Hủy"}
+        confirmLoading={isSubmit}
+      >
+        <Divider />
 
-    const productColumns = [
-        {
-            title: 'Id',
-            dataIndex: 'id',
-            key: 'id',
-            width: '10%',
-        },
-        {
-            title: 'Tên quyền',
-            dataIndex: 'name',
-            key: 'name',
-        },
-        {
-            title: 'API',
-            dataIndex: 'apiPath',
-            key: 'apiPath',
-        },
-        {
-            title: 'METHOD',
-            dataIndex: 'method',
-            key: 'method',
-        },
-        {
-            title: 'MODULE',
-            dataIndex: 'module',
-            key: 'module',
-        },
-        {
-            title: 'Thao tác',
-            key: 'action',
-            render: (_, record) => (
-                <DeleteTwoTone onClick={() => deleteProduct(record.key)} twoToneColor="#ff4d4f" />
-                // <Button type="link" onClick={() => deleteProduct(record.key)}>
-                //     Xóa
-                // </Button>
-            ),
-        },
-    ];
+        <Form
+          form={form}
+          name="create_role_form"
+          onFinish={onFinish}
+          layout="vertical"
+        >
+          <Form.Item
+            hidden
+            label="Id"
+            name="id"
+            rules={[{ required: true, message: "Vui lòng nhập id!" }]}
+          >
+            <Input />
+          </Form.Item>
 
-    return (
-        <>
+          <Form.Item
+            label="Tên role"
+            name="name"
+            rules={[{ required: true, message: "Vui lòng nhập tên role!" }]}
+          >
+            <Input />
+          </Form.Item>
 
-            <Modal
-                title="Cập nhật role"
-                open={openModalUpdate}
-                onOk={() => { form.submit() }}
-                onCancel={() => { setOpenModalUpdate(false); setProducts([]); form.resetFields(); setDataUpdate(null) }}
-                okText={"Tạo mới"}
-                cancelText={"Hủy"}
-                confirmLoading={isSubmit}
-            >
-                <Divider />
+          <Form.Item
+            label="Mô tả"
+            name="description"
+            rules={[{ required: true, message: "Vui lòng nhập mô tả!" }]}
+          >
+            <Input />
+          </Form.Item>
 
-                <Form
-                    form={form}
-                    name="create_role_form"
-                    onFinish={onFinish}
-                    layout="vertical"
-                >
-                    <Form.Item
-                        hidden
-                        label="Id"
-                        name="id"
-                        rules={[{ required: true, message: 'Vui lòng nhập id!' }]}
-                    >
-                        <Input />
-                    </Form.Item>
-
-                    <Form.Item
-                        label="Tên role"
-                        name="name"
-                        rules={[{ required: true, message: 'Vui lòng nhập tên role!' }]}
-                    >
-                        <Input />
-                    </Form.Item>
-
-                    <Form.Item
-                        label="Mô tả"
-                        name="description"
-                        rules={[{ required: true, message: 'Vui lòng nhập mô tả!' }]}
-                    >
-                        <Input />
-                    </Form.Item>
-
-
-                    {/* Bảng sản phẩm */}
-                    <Form.Item label="Danh sách quyền">
-                        <Button type="dashed" onClick={() => setModalAddPermission(true)}>
-                            Thêm quyền
-                        </Button>
-                        <Table
-                            dataSource={products}
-                            columns={productColumns}
-                            rowKey="name"
-                            pagination={false}
-                            style={{ marginTop: 16, overflowX: 'auto' }}
-                            bordered
-                        />
-                    </Form.Item>
-                </Form>
-            </Modal>
-            <ModalAddPermission
-                openModalAddPermission={openModalAddPermission}
-                setModalAddPermission={setModalAddPermission}
-                products={products}
-                setProducts={setProducts}
-                listPermissions={listPermissions}
+          {/* Bảng sản phẩm */}
+          <Form.Item label="Danh sách quyền">
+            <Button type="dashed" onClick={() => setModalAddPermission(true)}>
+              Thêm quyền
+            </Button>
+            <Table
+              dataSource={products}
+              columns={productColumns}
+              rowKey="name"
+              pagination={false}
+              style={{ marginTop: 16, overflowX: "auto" }}
+              bordered
             />
-        </>
-    );
+          </Form.Item>
+        </Form>
+      </Modal>
+      <ModalAddPermission
+        openModalAddPermission={openModalAddPermission}
+        setModalAddPermission={setModalAddPermission}
+        products={products}
+        setProducts={setProducts}
+        listPermissions={listPermissions}
+        permissionDataUpdate={permissionDataUpdate}
+      />
+    </>
+  );
 };
 
 export default RoleModalUpdate;
